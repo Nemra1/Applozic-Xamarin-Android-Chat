03a9cd158650f10924a3cb57723d794cbf4856b5
adarshmishra <adarsh@applozic.com>
2017-03-30 19:06:45 +0530

    #1 initial commit
    initial commit

---
+++ ApplozicChat/ApplozicChat/ApplozicFirebaseIIDService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+using Firebase.Messaging;
+using Com.Applozic.Mobicomkit.Api.Notification;
+using Applozic;
+
+namespace ApplozicChat
+{
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+	public class ApplozicFirebaseIIDService : FirebaseInstanceIdService
+	{
+		const string TAG = "MyFirebaseIIDService";
+	
+		public override void OnTokenRefresh()
+		{
+			var refreshedToken = FirebaseInstanceId.Instance.Token;
+			Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+
+			ApplozicChatManager ChatManger = new ApplozicChatManager(this);
+			ChatManger.SendRegistrationToServer(refreshedToken);
+		}
+
+	
+	}
+
+
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class MyFirebaseMessagingService : FirebaseMessagingService
+	{
+		const string TAG = "MyFirebaseMsgService";
+		public override void OnMessageReceived(RemoteMessage message)
+		{
+			Log.Debug(TAG, "From: " + message.From);
+			Log.Debug(TAG, "Notification Message Body: " + message.Data);
+
+			Log.Info(TAG, "Message data:" + message.Data);
+
+			if (message.Data.Count >0 )
+			{
+				if (MobiComPushReceiver.IsMobiComPushNotification(message.Data))
+				{
+					Log.Info(TAG, "Applozic notification processing...");
+					MobiComPushReceiver.ProcessMessageAsync(this, message.Data);
+					return;
+				}
+			}
+		}
+	}
+
+
+}
---
+++ ApplozicChat/ApplozicChat/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Applozic;
+using Firebase.Iid;
+using Android.Util;
+
+
+namespace ApplozicChat
+{
+	[Activity(Label = "Applozic Chat")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			ApplozicChatManager chatManager = new ApplozicChatManager(this);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button launchChatList = FindViewById<Button>(Resource.Id.launch_chat_list);
+			Button launchOneToOneChat = FindViewById<Button>(Resource.Id.launch_one_to_one_chat);
+			Button logout = FindViewById<Button>(Resource.Id.logout);
+
+			launchChatList.Click += delegate
+			{
+				Log.Debug("ApplozicChat", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+				chatManager.LaunchChatList();
+
+			};
+			launchOneToOneChat.Click += delegate
+			{
+
+				chatManager.LaunchChatWithUser("ak02");
+
+			};
+			logout.Click += delegate
+			{
+				UserLogoutListener logoutListener = new UserLogoutListener();
+				logoutListener.OnLogoutSucessHandler += (context) =>
+				{
+					context.StartActivity(typeof(LoginActivity));
+					this.Finish();
+				}; 
+				chatManager.Logout(logoutListener);
+
+			};
+
+		}
+	}
+
+}
+
